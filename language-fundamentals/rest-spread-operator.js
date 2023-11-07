1d927b5854a659fddb75aa4b2a9659f4778cb051
emanuelefavero <emafaux89@gmail.com>
2023-11-07 06:56:43 +0100

    add language-fundamentals/rest-spread-operator

---
+++ language-fundamentals/rest-spread-operator.js
@@ -0,0 +1,35 @@
+// * Rest Operator
+// The rest operator is used to represent an indefinite number of arguments as an array
+function sum(...args) {
+  // TIP: The rest operator converts the arguments into an array
+  return args.reduce((a, b) => a + b, 0)
+}
+
+console.log(sum(1, 2, 3)) // 6
+
+// TIP: The Rest Operator cannot be used as the first argument
+// function sum(...args, a) {} will throw an error
+
+// TIP: The Rest Operator can be used to get the remaining arguments
+function multiply(multiplier, ...args) {
+  return args.map((arg) => multiplier * arg)
+}
+
+console.log(multiply(2, 1, 2, 3)) // [2, 4, 6], since 2 is the multiplier and the rest of the arguments are multiplied by 2 (2 * 1, 2 * 2, 2 * 3)
+
+// * Spread Operator
+// The spread operator is used to expand an array into its individual elements
+let arr = [1, 2, 3]
+console.log(...arr) // 1 2 3
+
+// TIP: The spread operator can be used to copy an array
+let arr2 = [...arr]
+console.log(arr2) // [1, 2, 3]
+
+// NOTE: arr2 !== arr, since they are two different arrays. If you need to later check if two arrays are equal, you should assign them to variables and compare the variables instead (e.g. let arr3 = arr2)
+
+// TIP: The spread operator can be used to concatenate arrays
+let arr3 = [...arr, ...arr2]
+console.log(arr3) // [1, 2, 3, 1, 2, 3]
+
+// * QUESTIONS
