f6e801206e20e0db0950990b9b89a51e7a472078
emanuelefavero <emafaux89@gmail.com>
2023-11-11 16:32:53 +0100

    add language/fundamentals/iterables-iterators questions

--- language-fundamentals/iterables-iterators.js
+++ language-fundamentals/iterables-iterators.js
@@ -28,3 +28,16 @@ let obj = {
 for (let value of obj) {
   console.log(value) // 10, 11, 12
 }
+
+// QUESTIONS
+// Q: What is the purpose of the iterator?
+// A: The purpose of the iterator is to make ia data structure (such as a object) iterable so that we can use it in a for...of loop
+
+// Q: How do you create an iterator?
+// A: The Symbol.iterator method should return an iterator object with a next method that returns an object with two properties: done and value
+// Example: { done: false, value: 1 }
+
+// TIP: The `done: true` will stop the iteration
+
+// Q: explain a real world use case of iterators
+// A: For instance if you are creating a library and one of your object needs to be iterable, you can use the iterator to make it iterable
