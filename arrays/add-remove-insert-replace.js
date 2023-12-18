902ff8ab2f6a6148ee8568b4226f503aef87bc11
emanuelefavero <emafaux89@gmail.com>
2023-12-18 20:57:42 +0100

    add arrays/add-remove-insert-replace questions

--- arrays/add-remove-insert-replace.js
+++ arrays/add-remove-insert-replace.js
@@ -24,4 +24,26 @@ fruits[1] = 'pear'
 // or you can use splice
 fruits.splice(1, 1, 'coconut')
 
-console.log(fruits)
+console.log(fruits) // [ 'banana', 'coconut' ]
+
+// NOTE: the splice method modifies the original array, while the slice method returns a new array
+
+// * Slice - returns a sliced array
+let arr = ['a', 'b', 'c', 'd', 'e']
+let sliced = arr.slice(1, 3) // .slice(start, end)
+console.log(sliced) // [ 'b', 'c' ]
+
+// ------------------------------------------------------------
+// QUESTIONS
+
+// Q: What is the difference between push and unshift?
+// A: push adds an element to the end of the array, while unshift adds an element to the start of the array
+
+// Q: What is the difference between pop and shift?
+// A: pop removes an element from the end of the array, while shift removes an element from the start of the array
+
+// Q: How can you insert an element at a specific position in an array?
+// A: You can use the splice method (by passing 0 as the second argument and the element you want to insert as the third argument)
+
+// Q: How can you remove an element at a specific position in an array?
+// A: You can use the splice method (by passing 1 as the second argument)
