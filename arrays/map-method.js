cc718a31b09cd5fa343bb7a66305c4280187df83
emanuelefavero <emafaux89@gmail.com>
2023-12-20 04:36:21 +0100

    add arrays/map-method questions

--- arrays/map-method.js
+++ arrays/map-method.js
@@ -10,3 +10,41 @@ console.log(nums.map((num, index) => index)) // [0, 1, 2, 3], index of each elem
 
 // TIP: The third argument of the map method is the array itself:
 // .map((num, index, array) => ...)
+// TIP: The map method is often used to display data on a webpage (e.g. displaying a list of products) and to populate the DOM
+
+// ------------------------------------------------------------
+// QUESTIONS
+
+// Q: Find the length of each element in the array
+// A:
+
+let fruits = ['ab', 'abc', 'abcd']
+
+console.log(fruits.map((word) => word.length)) // [2, 3, 4]
+
+// Q: Find the square root of each element in the array and store it in a new array
+// A:
+
+let nums2 = [1, 2, 3, 4]
+
+console.log(nums2.map((num) => Math.sqrt(num)))
+// [1, 1.4142135623730951, 1.7320508075688772, 2]
+
+// Q: Fetch all the productName from the products array
+
+let products = [
+  {
+    price: 3,
+    productName: 'apple',
+  },
+  {
+    price: 4,
+    productName: 'banana',
+  },
+  {
+    price: 2,
+    productName: 'orange',
+  },
+]
+
+console.log(products.map((e) => e.productName)) // ['apple', 'banana', 'orange']
