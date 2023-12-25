c3acbcece69ef41181ea63c506dc308e88b5ab79
emanuelefavero <emafaux89@gmail.com>
2023-12-25 20:40:03 +0100

    add string/case-convention-and-replace replace method

--- string/case-convertion-and-replace.js
+++ string/case-convertion-and-replace.js
@@ -13,3 +13,17 @@ console.log(str2[0].toUpperCase() + str2.slice(1)) // 'Hello'
 // trim() removes whitespace from both ends of a string
 let str3 = ' hello '
 console.log(str3.trim()) // 'hello'
+
+// ------------------------------------------------------------
+// * replace()
+// replace() replaces a specified value with another value in a string and returns a new string
+
+let str4 = 'hello world'
+console.log(str4.replace('hello', 'ciao')) // 'ciao world'
+
+// TIP: replace() only replaces the first match
+// To replace all matches, use a regex with the global flag
+console.log(str4.replace(/l/g, 'x')) // 'hexxo worxd'
+
+// TIP: If you dont' pass a second argument, the value will be replaced with undefined
+console.log(str4.replace('hello')) // 'undefined world'
