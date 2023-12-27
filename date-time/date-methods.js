4646fb5cc61d618ebff63e63e151620649f9b28d
emanuelefavero <emafaux89@gmail.com>
2023-12-27 10:49:42 +0100

    add date-time/date-methods questions

--- date-time/date-methods.js
+++ date-time/date-methods.js
@@ -1,6 +1,8 @@
 let date = new Date()
 console.log(date) // 2023-12-27T09:12:59.648Z - date in UTC time
 console.log(date.toLocaleString()) // 12/27/2023, 10:24:20 AM - date in local time
+console.log(date.toLocaleDateString()) // 12/27/2023 - date in local time
+console.log(date.toLocaleTimeString()) // 10:24:20 AM - time in local time
 
 // TIP: .toLocaleString() is a good way to display dates to users since it will display the date in their local time
 
@@ -44,4 +46,19 @@ console.log(date) // 1703669259698
 date = new Date('2023-12-27T09:27:39.698Z')
 console.log(date.getTime()) // 1703669259698
 
-// TODO get day of the week name
+// ----------------------------------------------------------
+// QUESTIONS
+
+// Q: Get the name of the month
+// A: See date-time/basics.js getMonthName() function
+
+// Q: Find the date 90 days before the current date
+date = new Date()
+date.setDate(date.getDate() - 90)
+console.log(date) // 2023-09-28T08:43:40.190Z
+// A: You just need to set a new date object and subtract 90 days from the current date by using the .getDate() method
+
+// Q: What will bew the output if you add 32 as date in Date() constructor?
+date = new Date(2007, 0, 32)
+console.log(date.toLocaleString()) // 2/1/2007, 12:00:00 AM
+// A: You will get the date for February 1st, 2007 since the date is out of range for January by 1 day
