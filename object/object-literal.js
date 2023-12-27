088c9885c7f9ff54703f0a965be091dacca7df5d
emanuelefavero <emafaux89@gmail.com>
2023-12-27 23:36:08 +0100

    add object/object-literal dynamic property

--- object/object-literal.js
+++ object/object-literal.js
@@ -1,18 +1,28 @@
 // * OBJECT LITERAL
 
+let property = 'Age'
+
 let user = {
   // * Property
   name: 'John',
   age: 32,
 
-  // * Method
+  // * Method (es6 syntax)
   getName() {
     return user.name
   },
+
+  // * Dynamic property (es6)
+  // You can use square brackets in an object literal to create a dynamic property
+  // The value of the property is evaluated as an expression
+  ['get' + property]: () => {
+    return user.age
+  },
 }
 
-console.log(user) // { name: 'John', age: 32, getName: [Function: getName] }
+console.log(user) // { name: 'John', age: 32, getName: [Function: getName], getAge: [Function: getAge] }
 console.log(user.getName()) // John
+console.log(user.getAge()) // 32
 
 // * convert object to JSON
 let json = JSON.stringify(user)
