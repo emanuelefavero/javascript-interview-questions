2fbcfa2fe259b3552f31958ed17b296b1bbaeca4
emanuelefavero <emafaux89@gmail.com>
2023-12-29 14:03:38 +0100

    add object/class-inheritance questions

--- object/class-inheritance.js
+++ object/class-inheritance.js
@@ -26,3 +26,25 @@ class Cat extends Animal {
 let cat = new Cat('Tom', 3)
 console.log(cat.getName()) // Tom, call the parent method
 console.log(cat.getNameAndAge()) // Tom is 3 years old, call the parent method from the child class
+
+// ------------------------------------------------------------
+// QUESTIONS
+
+// Q: How can you inherit a class?
+// A: You can use the extends keyword
+
+// Q: What is sub classing?
+// A: Sub classing is the same as inheritance
+
+// Q: What is super?
+// A: super is used to call the parent constructor or parent methods
+
+// Q: How can you override a method?
+// A: With the super keyword
+
+// Q: How can you extend a built-in class?
+class Child extends String {}
+let child = new Child('Hello')
+console.log(child.toLowerCase()) // hello, call the built-in method toLowerCase() from the parent class String
+
+// A: You can use the extends keyword
