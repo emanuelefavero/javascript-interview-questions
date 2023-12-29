c2cf6f3b8c4717b98d7d1b5b079a4bdf6f5bc842
emanuelefavero <emafaux89@gmail.com>
2023-12-29 10:13:26 +0100

    add object/call-apply-bind

---
+++ object/call-apply-bind.js
@@ -0,0 +1,42 @@
+// * Call, Apply, Bind
+// Call, Apply and Bind are used to change the `this` value of a function
+
+// * Call
+// Call method is used to call a function with a given this value and arguments provided individually
+
+let obj = {
+  a: 2,
+}
+
+function test() {
+  console.log(this) // { a: 2 }
+  console.log(this.a) // 2
+}
+
+test.call(obj) // { a: 2 }, call test() with obj as this value
+
+// NOTE: You can pass arguments to the function as well
+function test2(b) {
+  console.log(this.a + b)
+}
+
+test2.call(obj, 3) // 5, call test2() with obj as this value and 3 as argument
+
+// * Apply
+// Apply method is similar to call() but it takes arguments as an array
+
+function test3(b, c) {
+  console.log(this.a + b + c)
+}
+
+test3.apply(obj, [3, 2]) // 7, call test3() with obj as this value and [3, 2] as arguments
+
+// * Bind
+// Bind method is used to create a new function with a given this value and arguments provided individually
+
+function test4(b) {
+  console.log(this.a + b)
+}
+
+let add = test4.bind(obj) // create a new function with obj as this value
+add(4) // 6, call test4() with obj as this value and 4 as argument
