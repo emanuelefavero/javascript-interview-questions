b61c57adf7fb626d85611897c41e76f7b2bff41e
emanuelefavero <emafaux89@gmail.com>
2023-12-29 14:52:49 +0100

    add object/getter-setter create read-only properties

--- object/getter-setter.js
+++ object/getter-setter.js
@@ -26,3 +26,24 @@ let user = new User('John')
 console.log(user.name) // JOHN, the getter is executed when we read user.name so we get the uppercase name
 
 // TIP: We can't see it but in this example the name is stored in lowercase by the setter
+
+// ------------------------------------------------------------
+// * You can create a read-only property by omitting the setter
+
+class User2 {
+  constructor(name) {
+    this._name = name
+  }
+
+  get name() {
+    return this._name.toUpperCase()
+  }
+}
+
+let user2 = new User2('John')
+
+user2.name = 'Jack' // NOTE: try to change the name
+console.log(user2.name) // JOHN, still the same
+
+// ? Why is the name still the same?
+// Because we didn't define a setter so the name can't be changed
