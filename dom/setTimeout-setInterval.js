2f748800809a71ac2754ebac1341b526cb23a308
emanuelefavero <emafaux89@gmail.com>
2024-01-02 03:24:09 +0100

    add dom/setTimeout-setInterval questions

--- dom/setTimeout-setInterval.js
+++ dom/setTimeout-setInterval.js
@@ -8,3 +8,15 @@ setTimeout(() => {
     }
   }, 500)
 }, 200)
+
+// -----------------------------
+// QUESTION
+
+// Q: What is the difference between setTimeout and setInterval?
+// A: setTimeout runs a function once after a specified delay. setInterval runs a function repeatedly with a specified interval between each call.
+
+// Q: Why can't we write document.setTimeout?
+// A: Because setTimeout is a method of the window object, not the document object.
+
+// Q: How can you stop a timer?
+// A: With clearInterval or clearTimeout
