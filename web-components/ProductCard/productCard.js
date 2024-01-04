32ad7967836468843d5898951ac701b3baafad32
emanuelefavero <emafaux89@gmail.com>
2024-01-04 09:14:39 +0100

    add web-components/ dynamic data

--- web-components/ProductCard/productCard.js
+++ web-components/ProductCard/productCard.js
@@ -3,47 +3,64 @@ const template = document.createElement('template')
 template.innerHTML = `
 <style>
   .card {
-    box-shadow: 0 4px 8px 0 rgba(203, 213, 225, 0.8);
+    box-shadow: 0 3px 5px 0 rgba(203, 213, 225, 0.8);
     padding: 1rem;
     margin: 1rem;
-    border-radius: 0.4rem;
+    border-radius: 0.5rem;
     user-select: none;
 
-    h3 {
-      margin-bottom: -.2rem;
-      user-select: text;
-
-      .price {
-        user-select: none;
-        color: #059669;
-      }
+    &:hover {
+      background-color: rgba(241, 245, 249, 0.4);
+      box-shadow: 0 4px 6px 0 rgba(203, 213, 225, 0.9);
     }
+
+    img {
+      padding: 0.5rem;
+    }
+
+    h3 {
+      user-select: text;
+      position: relative;
+      top: 0.3rem;
+
+        .price {
+            user-select: none;
+            margin-left: 0.2rem;
+            color: #059669;
+        }
+      }
+
 
     button {
       background-color: #059669;
       color: #fff;
-      border: none;
+      border: 1px solid #10b981;
       padding: 0.5rem 1rem;
       border-radius: 0.4rem;
       cursor: pointer;
       text-transform: uppercase;
+      transition: background-color 0.15s ease-in;
 
       &:hover {
         background-color: #10b981;
       }
-    }
-  }
 
-  .card:hover {
-    box-shadow: 0 6px 12px 0 rgba(203, 213, 225, 0.9);
+      &:active {
+        background-color: #047857;
+        scale: 0.98;
+      }
+    }
   }
 </style>
 
 <div class="card">
-  <img src="./images/banana.png" alt="fruit" width='150' />
+  <img width='130' />
   <div class="container">
-    <h3>Banana<span class='price'>&nbsp;$0.50</span></h3>
-    <p>Not Sold</p>
+    <h3>
+      <span class='product-name'></span>
+      <span class='price'></span>
+    </h3>
+    <p></p>
     <button class='btn'>Buy</button>
   </div>
 </div>
@@ -52,9 +69,24 @@ template.innerHTML = `
 class ProductCard extends HTMLElement {
   constructor() {
     super()
+
+    // Create shadow DOM
     this.attachShadow({ mode: 'open' })
+
+    // Add template
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+    // Add dynamic data
+    this.shadowRoot.querySelector('.product-name').innerText =
+      this.getAttribute('productName')
+    this.shadowRoot.querySelector('.price').innerText =
+      this.getAttribute('price')
+    this.shadowRoot.querySelector('img').src = this.getAttribute('image')
+    this.shadowRoot.querySelector('img').alt = this.getAttribute('productName')
+    this.shadowRoot.querySelector('p').innerText =
+      this.getAttribute('productStatus')
   }
 }
 
+// Define the new HTML element
 customElements.define('product-card', ProductCard)
