1c5fb498e0d977ca71c8e2d792d45265c5fd2d54
emanuelefavero <emafaux89@gmail.com>
2024-01-06 09:05:09 +0100

    add asynchronous/promise QUESTIONS

--- asynchronous/promise.js
+++ asynchronous/promise.js
@@ -34,3 +34,18 @@ sqrt(2)
   .then((result) => {
     console.log(result) // 16
   })
+
+// ------------------------------------------------------------
+// QUESTIONS
+
+// Q: How can you run a piece of code at the end of a promise chain?
+// A: By using the finally method
+
+// Q: How can you chain promises?
+// A: By returning a promise inside a then
+
+// Q: What is status of a promise initially?
+// A: Pending. When a promise is created, it is in the pending state. This means that the outcome of the promise is unknown at that point in time.
+
+// Q: What will be the status of promise when resolved or rejected?
+// A: Resolved or rejected
