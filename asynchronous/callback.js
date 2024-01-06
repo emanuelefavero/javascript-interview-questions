10ca42355275f97b0497114bfb0a89c951ac6e75
emanuelefavero <emafaux89@gmail.com>
2024-01-06 08:40:48 +0100

    add asynchronous/callback QUESTIONS

--- asynchronous/callback.js
+++ asynchronous/callback.js
@@ -25,3 +25,15 @@ sqrt(2, (result) => {
 
 // TIP: These days callbacks are not used much. Instead, we use promises and async/await.
 // TIP: Callback hell - when we have a lot of nested callbacks. This is a common problem with callbacks
+
+// ------------------------------------------------------------
+// QUESTIONS
+
+// Q: What is an asynchronous process?
+// A: An asynchronous process is one that is executed in the background and does not block the main thread.
+
+// Q: What is callback hell?
+// A: Callback hell is when we have a lot of nested callbacks.
+
+// Q: Why is callback hell also called pyramid of doom?
+// A: Because of the pyramid-like shape of the code (too much indentation and nested code)
