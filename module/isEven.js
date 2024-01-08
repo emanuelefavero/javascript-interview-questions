e429d8b753e09b5f8b4e83f957ac36b3657d0724
emanuelefavero <emafaux89@gmail.com>
2024-01-08 03:55:27 +0100

    add module/* QUESTIONS

--- module/isEven.js
+++ module/isEven.js
@@ -1,3 +1,7 @@
+// This is how you export a function
 export function isEven(n) {
   return n % 2 === 0
 }
+
+// TIP: If you want to export a single function, you can use the default keyword
+// export default function ...
