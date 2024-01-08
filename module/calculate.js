0ec0a69018099f15c6689d1f78c3b99aa125ce23
emanuelefavero <emafaux89@gmail.com>
2024-01-08 04:43:41 +0100

    add module/* default export

--- module/calculate.js
+++ module/calculate.js
@@ -1,3 +1,6 @@
 export default function sum(a, b) {
   return a + b
 }
+
+// TIP: You can also set a default export like this:
+// export { sum as default }
---
+++ module/subtract.js
@@ -0,0 +1,2 @@
+// TIP: You can also export an unnamed function like this:
+export default (a, b) => a - b
