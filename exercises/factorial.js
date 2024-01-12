009edd43e87e0961532c3b830d31d964e239a367
emanuelefavero <emafaux89@gmail.com>
2024-01-12 06:50:57 +0100

    add exercises/factorial

---
+++ exercises/factorial.js
@@ -0,0 +1,12 @@
+// * Get the factorial of a number
+
+// TIP: The factorial of a number is the product of all the integers from 1 to that number. For example, the factorial of 4 is 24, since 4 * 3 * 2 * 1 = 24
+
+// O(n) time, O(1) space
+function factorial(n) {
+  if (n <= 1) return 1
+
+  return n * factorial(n - 1)
+}
+
+console.log(factorial(4)) // 24, since 4 * 3 * 2 * 1 = 24
