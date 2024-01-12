6257fd99284d4f358ac1d3a689b8d86c49b9fe57
emanuelefavero <emafaux89@gmail.com>
2024-01-12 22:19:34 +0100

    add summary/unicode

---
+++ summary/unicode.js
@@ -0,0 +1,37 @@
+// unicode characters
+
+// * Numbers
+console.log('\u0030') // 0
+console.log('\u0031') // 1
+
+// * Letters
+console.log('\u0041') // A
+console.log('\u0061') // a
+
+// * Greek
+console.log('\u0394') // Δ
+console.log('\u039b') // Λ
+console.log('\u03a3') // Σ
+console.log('\u03a9') // Ω
+console.log('\u03c0') // π
+
+// * Arabic, Hebrew
+console.log('\u06a1') // ڡ
+console.log('\u05d4') // ה
+
+// * Shapes
+console.log('\u2588') // █
+console.log('\u25a0') // ■
+console.log('\u25a1') // ■
+console.log('\u25b2') // ▲
+console.log('\u25b3') // △
+console.log('\u25d2') // ◒
+
+// * Games
+console.log('\u{1f076}') // 🁶
+console.log('\u{1f0a6}') // 🂦
+
+// * Emoji
+console.log('\u{1F680}') // 🚀
+
+// TIP: The syntax for long unicode characters is \u{code}
