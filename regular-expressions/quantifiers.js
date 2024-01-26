51229e7a9ae19e1d193aea79215293bf949727cd
emanuelefavero <emafaux89@gmail.com>
2024-01-26 22:19:54 +0100

    add regular-expressions/quantifiers - {n} quantifier

--- regular-expressions/quantifiers.js
+++ regular-expressions/quantifiers.js
@@ -2,16 +2,28 @@
 
 // The quantifiers specify how many occurrences of a character, group, or character class must be present in the input for a match to be found
 
-// * matches between zero and unlimited times
+// * * matches between 0 and unlimited times
 
 let string = 'This is a kiss, I...'
 let regex = /is*/gi // ? match 'i' followed by zero or more 's'
 console.log(string.match(regex)) // [ 'is', 'is', 'iss', 'I' ]
 
-// + matches between one and unlimited times
+// * + matches between 1 and unlimited times
 regex = /is+/gi // ? match 'i' followed by one or more 's'
 console.log(string.match(regex)) // [ 'is', 'is', 'iss' ]
 
-// ? matches between zero and one time
+// * ? matches between 0 and 1 time
 regex = /is?/gi // ? match 'i' followed by zero or one 's'
 console.log(string.match(regex)) // [ 'is', 'is', 'is', 'I' ]
+
+// * {n} matches exactly n times
+regex = /is{2}/gi // ? match 'i' followed by exactly two 's'
+console.log(string.match(regex)) // [ 'iss' ]
+
+// * {n,} matches at least n times
+regex = /is{2,}/gi // ? match 'i' followed by at least two 's'
+console.log(string.match(regex)) // [ 'iss' ]
+
+// * {n,m} matches at least n times but no more than m times
+regex = /is{1,2}/gi // ? match 'i' followed by one to two 's'
+console.log(string.match(regex)) // [ 'is', 'is', 'iss' ]
