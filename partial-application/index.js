b7995bde0f49a51a8699cdbae4805b78caceb6cf
emanuelefavero <emafaux89@gmail.com>
2024-01-29 02:10:10 +0100

    add partial-application/index example 2, 3

--- partial-application/index.js
+++ partial-application/index.js
@@ -12,3 +12,31 @@ function addFive(...others) {
 }
 
 console.log(addFive(2, 3)) // 10
+
+// ------------------------------------------------------------
+// Example 2
+
+function multiply(a, b) {
+  return a * b
+}
+
+function timesTwo(b) {
+  return multiply(2, b)
+}
+
+console.log(timesTwo(10)) // 20
+
+// ------------------------------------------------------------
+// Example 3
+
+function subtract(a, b, c) {
+  return a - b - c
+}
+
+function subtractFiveAndTwo(a) {
+  return subtract(a, 5, 2)
+}
+
+console.log(subtractFiveAndTwo(10)) // 3
+// Explanation: subtractFiveAndTwo(10) is equivalent to subtract(10, 5, 2)
+// 10 - 5 - 2 = 3
