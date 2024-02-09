cc92577aca685c2f06c0c79b83e300b2d2211507
emanuelefavero <emafaux89@gmail.com>
2024-02-09 06:34:22 +0100

    add .../assignments/validate-date-format validate value

--- regular-expressions/assignments/validate-date-format.js
+++ regular-expressions/assignments/validate-date-format.js
@@ -15,3 +15,15 @@ regex = new RegExp(`^${month}/${day}/${year}$`)
 console.log(regex.test('10/16/1960')) // true
 console.log(regex.test('1/16/1960')) // true
 console.log(regex.test('99/1/a')) // false
+
+// * Validate value in date format yyyy-mmm-dd
+year = '(19|20)[0-9]{2}'
+month = '(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)'
+day = '(0[1-9]|[12][0-9]|3[01])'
+
+regex = new RegExp(`^${year}-${month}-${day}$`, 'i') // i flag for case insensitive
+
+console.log(regex.test('2019-jan-01')) // true
+console.log(regex.test('2019-Jan-01')) // true
+console.log(regex.test('2019-jan-32')) // false
+console.log(regex.test('2019-jan-1')) // false
