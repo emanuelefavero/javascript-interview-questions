7d9f3cf707904be301dd208be3528715f7c57940
emanuelefavero <emafaux89@gmail.com>
2024-02-09 06:08:23 +0100

    add .../assignments/validate-date-format split in variables

--- regular-expressions/assignments/validate-date-format.js
+++ regular-expressions/assignments/validate-date-format.js
@@ -5,3 +5,13 @@ let regex = /^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/
 console.log(regex.test('10/16/1960')) // true
 console.log(regex.test('1/16/1960')) // true
 console.log(regex.test('99/1/a')) // false
+
+// * Split the regex in variables so it easier to read
+let month = '[0-9]{1,2}'
+let day = '[0-9]{1,2}'
+let year = '[0-9]{4}'
+regex = new RegExp(`^${month}/${day}/${year}$`)
+
+console.log(regex.test('10/16/1960')) // true
+console.log(regex.test('1/16/1960')) // true
+console.log(regex.test('99/1/a')) // false
