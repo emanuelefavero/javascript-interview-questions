7151b596a1b8d31119a94f0d9e857ddf36930918
Emanuele Favero <emafaux89@gmail.com>
2025-06-27 02:48:46 +0200

    refactor: add display-current-date exercise

--- exercises/basic/display-current-date.js
+++ exercises/basic/display-current-date.js
@@ -1,7 +1,7 @@
 // * Display current date and time
 
-/*
-Sample output: Today is: Tuesday
+/* Sample output:
+Today is: Tuesday
 Current time is: 10 PM:30:38
 */
 
@@ -20,29 +20,19 @@ function currentDateAndTime() {
   ]
   let weekDay = weekDays[now.getDay()]
 
-  // Get hours
+  // Get time data
   let hours = now.getHours()
-
-  // Get AM/PM
   let amOrPm = hours >= 12 ? 'PM' : 'AM'
-
-  // Get minutes
   let minutes = now.getMinutes()
-
-  // Get seconds
   let seconds = now.getSeconds()
 
-  // Format time
-  function format(time) {
-    return time.toString().padStart(2, '0')
-  }
+  // Format time utility
+  const format = (time) => time.toString().padStart(2, '0')
 
   // Formatted time
   let hours12 = hours % 12 || 12 // convert 0 -> 12
   let fMinutes = format(minutes)
   let fSeconds = format(seconds)
-
-  // Build Clock
   let clock = `${hours12} ${amOrPm}:${fMinutes}:${fSeconds}`
 
   // Output
