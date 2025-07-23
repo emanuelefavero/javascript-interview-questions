281e4c00f0192b43406d6b777516a636e13dd884
Emanuele Favero <emafaux89@gmail.com>
2025-07-23 15:07:23 +0200

    fix: account for leap years in days-left-before-christmas

--- exercises/basic/days-left-before-christmas.js
+++ exercises/basic/days-left-before-christmas.js
@@ -3,14 +3,13 @@ function daysLeftBeforeChristmas() {
   const currentYear = today.getFullYear()
   const christmas = new Date(`${currentYear}-12-25`)
 
+  // ? If today is after Christmas, set Christmas to next year
+  if (today > christmas) christmas.setFullYear(currentYear + 1)
+
   const diff = christmas.getTime() - today.getTime()
 
   // ? Use ceil to round up the number of days, since we want to count partial days as a full day left
   const days = Math.ceil(diff / 1000 / 60 / 60 / 24)
-
-  // ? If today is after Christmas, the days will be negative (until Dec 31). So we can simply add 365 to get the days left until next Christmas (e.g. -6 + 365 = 359 days left)
-  if (days < 0) return days + 365
-
   return days
 }
 
