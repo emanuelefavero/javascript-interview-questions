c6369dfc61e2e507b666b82e59ea116af4362b65
Emanuele Favero <emafaux89@gmail.com>
2025-07-23 15:09:06 +0200

    docs: udpate comments in days-left-before-christmas

--- exercises/basic/days-left-before-christmas.js
+++ exercises/basic/days-left-before-christmas.js
@@ -3,12 +3,12 @@ function daysLeftBeforeChristmas() {
   const currentYear = today.getFullYear()
   const christmas = new Date(`${currentYear}-12-25`)
 
-  // ? If today is after Christmas, set Christmas to next year
+  // If today is after Christmas, set Christmas to next year
   if (today > christmas) christmas.setFullYear(currentYear + 1)
 
   const diff = christmas.getTime() - today.getTime()
 
-  // ? Use ceil to round up the number of days, since we want to count partial days as a full day left
+  // ? Since we want to count partial days as a full day left, we use Math.ceil
   const days = Math.ceil(diff / 1000 / 60 / 60 / 24)
   return days
 }
