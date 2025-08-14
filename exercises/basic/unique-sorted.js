5b7aaa726e41b2bed3ecc91a34457cb125c4b542
Emanuele Favero <emafaux89@gmail.com>
2025-08-15 00:46:35 +0200

    feat: add exercises/basic/unique-sorted variation

--- exercises/basic/unique-sorted.js
+++ exercises/basic/unique-sorted.js
@@ -14,3 +14,31 @@ function uniqueSorted(nums) {
 }
 
 console.log(uniqueSorted([4, 2, 5, 2, 1, 4])) // [1, 2, 4, 5]
+
+// -----------------
+// VARIATION
+
+/*
+Write a function that takes two arrays and returns a sorted array containing all numbers that:
+
+Appear in both arrays (intersection).
+
+Have no duplicates in the result.
+
+Are sorted in ascending order.
+*/
+
+function intersectionSorted(nums1, nums2) {
+  let set1 = new Set([...nums1])
+  let set2 = new Set([...nums2])
+  let result = []
+
+  set1.forEach((num) => {
+    if (set2.has(num)) result.push(num)
+  })
+
+  return result.sort((a, b) => a - b)
+}
+
+console.log(intersectionSorted([4, 2, 5, 2, 1, 4], [5, 3, 4, 4, 2]))
+// [2, 4, 5]
