249e8fdf02be19901c740594a633b30d19ec90de
Emanuele Favero <emafaux89@gmail.com>
2025-08-16 01:28:18 +0200

    feat: add exercises/basic/array-chunking

---
+++ exercises/basic/array-chunking.js
@@ -0,0 +1,18 @@
+// * Array Chunking
+
+// Write a function that takes an array and a chunk size, and returns a new array where the original array is split into subarrays of the given size.
+
+// TIP: In-place solution
+
+function chunkArray(array, size) {
+  let result = []
+
+  while (array.length) {
+    result.push(array.splice(0, size))
+  }
+
+  return result
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5], 2)) // [[1, 2], [3, 4], [5]]
+console.log(chunkArray([1, 2, 3, 4, 5, 6], 3)) // [[1, 2, 3], [4, 5, 6]]
