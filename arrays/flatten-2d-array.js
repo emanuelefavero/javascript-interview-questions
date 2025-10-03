9e226f95fbcdb4ea04ca011196c624b7e35e1e44
Emanuele Favero <emafaux89@gmail.com>
2025-10-03 17:49:24 +0200

    feat: add arrays/flatten-2d-array solution 3

--- arrays/flatten-2d-array.js
+++ arrays/flatten-2d-array.js
@@ -16,3 +16,16 @@ function flatten2(array) {
   return array.flat(1)
 }
 console.log(flatten2(array)) // [1, 2, 3, 4]
+
+// 3 - With for loop
+function flatten3(array) {
+  let result = []
+
+  for (const item of array) {
+    if (Array.isArray(item)) result.push(...item)
+    else result.push(item)
+  }
+
+  return result
+}
+console.log(flatten3(array)) // [1, 2, 3, 4]
