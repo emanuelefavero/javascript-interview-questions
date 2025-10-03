35cec622d07cb2d29399cafc07b5e73c29e0e763
Emanuele Favero <emafaux89@gmail.com>
2025-10-03 17:47:16 +0200

    feat: add arrays/flatten-2d-array solution 2

--- arrays/flatten-2d-array.js
+++ arrays/flatten-2d-array.js
@@ -5,8 +5,14 @@ let array = [
   [3, 4],
 ]
 
+// 1 - With reduce
 function flatten(array) {
   return array.reduce((acc, current) => [...acc, ...current], [])
 }
-
 console.log(flatten(array)) // [1, 2, 3, 4]
+
+// 2 - With built-in `flat` method
+function flatten2(array) {
+  return array.flat(1)
+}
+console.log(flatten2(array)) // [1, 2, 3, 4]
