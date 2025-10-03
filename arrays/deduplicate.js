bec41c0bf94ce3a4bcb7f50ed1f55a37c239930e
Emanuele Favero <emafaux89@gmail.com>
2025-10-03 18:37:06 +0200

    feat: add arrays/deduplicate solution 2

--- arrays/deduplicate.js
+++ arrays/deduplicate.js
@@ -9,4 +9,18 @@ function deduplicate(array) {
   const set = new Set([...array])
   return [...set]
 }
-console.log(deduplicate(array))
+console.log(deduplicate(array)) // [1, 2, 3]
+
+// 2 - With a map
+function deduplicate2(array) {
+  let map = new Map()
+  let result = []
+
+  for (const num of array) {
+    if (!map.has(num)) result.push(num)
+    map.set(num, true)
+  }
+
+  return result
+}
+console.log(deduplicate2(array)) // [1, 2, 3]
