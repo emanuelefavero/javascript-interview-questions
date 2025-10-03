c043f447b9176b7f651a4e72270cdbafaea45035
Emanuele Favero <emafaux89@gmail.com>
2025-10-03 18:03:32 +0200

    feat: add arrays/flatten-deep-array solution 2

--- arrays/flatten-deep-array.js
+++ arrays/flatten-deep-array.js
@@ -9,3 +9,15 @@ function flatten(array) {
   return array.flat(Infinity)
 }
 console.log(flatten(array)) // [1, 2, 3, 4, 5, 6]
+
+// 2 - With Recursion
+function flatten2(array, result = []) {
+  for (const item of array) {
+    if (Array.isArray(item)) {
+      flatten2(item, result)
+    } else result.push(item)
+  }
+  return result
+}
+
+console.log(flatten2(array))
